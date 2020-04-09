6a7c5e5640a767bdcd0cdfb9eb9aefcf54b051e3
sblendo00 <chri.sble@libero.it>
2020-04-09 19:19:44 +0000

    sistemato anche back

--- Angular-Spotify/src/app/app-routing.module.ts
+++ Angular-Spotify/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import { TrackComponent } from './track/track.component';
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'tracks/:id', component: TrackComponent }
+];
 
-
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular-Spotify/src/app/spotify.service.ts
+++ Angular-Spotify/src/app/spotify.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const headers = new HttpHeaders({Authorization: environment.oauthToken});
 
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
@@ -21,4 +24,14 @@ export class SpotifyService {
     return obsTracks;
  //Ritorno un observable ai componenti che richiedono il servizio
   }
+getTrack(id: string) {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer TUO_AUTH'
+    });
+
+    return this.http.get(url, { headers });
+  }
+
 }
